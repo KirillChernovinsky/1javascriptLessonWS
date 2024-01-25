634e9b3f0a8f3b94fa4d7122e53e84df2a5841e3
Kirill Chernovinskii <kirillchernovinsky@yandex.ru>
2024-01-26 01:20:40 +0300

    commm

---
+++ cyclesANDfunctions.js
@@ -0,0 +1,171 @@
+// let num = 0;
+// while (num < 5) {
+//     console.log(num);
+//     num++;
+// }
+
+// let num = 1
+// while (num <= 100){
+//     console.log(num)
+//     num++
+// }
+
+
+
+// let num = 11
+// while (num <= 33){
+//     console.log(num)
+//     num++
+// }
+//
+//
+// let num = 1;
+// while(num <= 100){
+//     if (num % 2 === 0){
+//         console.log(num)
+//         num++
+//     }else {
+//         num++
+//     }
+// }
+
+
+// let num = 1
+// let massa = 0
+// while(num <= 100){
+//     massa = num + massa
+//     num++
+// } console.log(massa)
+
+
+// for (let num = 0; num < 5; num++) {
+//     console.log(num);
+// }
+//
+
+// let array = [1, 2, 3, 4, 5]
+//
+// for (let i = 0; i < array.length; i++){
+//     console.log(array[i])
+// }
+
+
+
+
+
+// let array = [1, 2, 3, 4, 5]
+// let result = 0
+//
+// for (let i = 0; i < array.length; i++){
+//     result = result + array[i]
+// }console.log(result)
+
+
+// var obj = {green: 'зелёный', red: 'красный', blue: 'голубой'}
+// for (var key in obj){
+//     console.log(key)
+// }
+
+// крутая задачка
+// var obj = {Kolja: '200', Vasya: '300', Petya: '400'}
+// for (var key in obj){
+//     console.log(`${key}- зарплата ${obj[key]} долларов.`)
+// }
+
+
+// тоже интересная
+// var massiv = [2, 5, 9, 15, 0, 4]
+// for (let i = 0; i < massiv.length; i++){
+//     if (massiv[i] > 3 && massiv[i] < 10){
+//         console.log(massiv[i])
+//     }
+// }
+
+
+// var massiv = [-2, 2, 3]
+// result = 0
+// for (let i=0; i < massiv.length; i++){
+//     if (massiv[i] > 0){
+//         result = result + massiv[i]
+//     }
+// }console.log(result)
+
+
+// var massiv = [1, 2, 5, 6, 2, 1, 4]
+// for (let i =0; massiv.length; i++){
+//     if (massiv[i] === 4){
+//         console.log('Есть!')
+//         break;
+//     }
+// }
+
+
+// var massiv = [10, 20, 30, 50, 235, 3000]
+// for (let i=0; massiv.length; i++){
+//     if (massiv[i] === 1 || massiv[0] === 2 || massiv[0] === 5){
+//
+//     }
+// }
+
+// let massiv = [10, 20, 30, 50, 235, 3000];
+//
+// for (let i=0; massiv.length; i++){
+//     let first_element = massiv[i].toString();
+//     let first_digit = parseInt(first_element[0]);
+//     if (first_digit === 1 || first_digit === 2 || first_digit === 5){
+//         console.log(first_element)
+//     } else {
+//         console.log('все')
+//     }
+// }
+
+
+
+// let massiv = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+//
+// for (let i=0; i < massiv.length; i++){
+//     console.log(`-${massiv[i]}`)
+// }
+
+//
+// var daysOfWeek = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+// for (var i = 0; i < daysOfWeek.length; i++) {
+//     if (i === 5 || i === 6) {
+//         document.write("<b>" + daysOfWeek[i] + "</b>");
+//     } else {
+//         document.write(daysOfWeek[i] + "");
+//     }
+// }
+//
+
+
+
+// var weekDays = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+// // Выводим дни недели с помощью цикла for
+// for (var i = 0; i < weekDays.length; i++) {
+//     if (i === 0){
+//         document.write('<i>' + weekDays[i] + '</i>');
+//     }else {
+//         document.write(weekDays[i]);
+//     }
+// }
+//
+// let num = 1;
+// while(num <= 100){
+//     if (num % 2 === 0){
+//         console.log(num)
+//         num++
+//     }else {
+//         num++
+//     }
+// }
+//
+
+// тоже крутая задачка
+// let n=1000
+// num = 0
+// while (n > 50){
+//     n = n / 2
+//     num++
+// } console.log(n)
+// console.log(num)
